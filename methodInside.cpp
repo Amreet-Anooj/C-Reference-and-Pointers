064df2a25e7ccf5c81b53d96514ab61ca2c9ab32
Amreet-Anooj <amreet789@gmail.com>
2024-10-15 13:40:39 +0530

    Add files via upload

---
+++ methodInside.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Student               
+{
+    public:
+    void myMethod()
+    {
+        cout<<"I am studying in 2nd year";
+    }
+};
+
+int main()
+{
+    Student s1;
+    s1.myMethod();
+}
+
+
+/*Output:
+I am studying in 2nd year*/
---
+++ methodOutside.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    public:
+    void myMethod();
+};
+
+void Student::myMethod()
+{
+    cout<<"A Sophomore in SIT";
+}
+
+int main()
+{
+    Student s1;
+    s1.myMethod();
+}
+
+
+/*Output:
+A Sophomore in SIT*/
